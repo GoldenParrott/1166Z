c8d2798209d23a556ab8758eb9dc2ff5a3939fe4
Durzogg <caleb.miller@jdmsoft.com>
2024-07-19 10:33:00 -0600

    Friday Changes 1 - Organized Functions into CPP Files
    -- functions.cpp organizes miscellaneous functions and tasks
    -- autons.cpp has all of the autonomous routines in different functions

---
+++ src/functions.cpp
@@ -0,0 +1,39 @@
+#include "init.h"
+
+
+void colorSensorOn() {
+	int colorDelayTask;
+	while (true) {
+		//                       < 020
+		if (colorSense.get_hue() < 20) {
+			Eject.set_value(true);
+			colorDelayTask = 1;
+		} else if (colorDelayTask >= 500) {
+			Eject.set_value(false);
+			colorDelayTask = 0;
+		}
+		if (colorDelayTask != 0) {
+			colorDelayTask += 20;
+		}
+		pros::delay(20);
+	}
+}
+
+void raiseArm() {
+	UpLeft.tare_position();
+	IntakePTO.move(-128);
+	waitUntil(abs(UpLeft.get_position()) >= 1700);
+	IntakePTO.brake();
+}
+
+
+
+
+void transportThenGripTASK() {
+	Transport.move(-128);
+	// Odometry again
+	// 	      currentMotorReading     gearRatio    wheelCirc  wheelRev singleDeg setPoint
+	//                |					  |             |        |        |        |
+	waitUntil(BackRight.get_position() * 0.75 * (((3.25 * 3.14) * 2.54) / 360) >= 33);
+	GrabPiston.set_value(true);
+}
