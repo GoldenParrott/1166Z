34f7db9b5a0446f4e0adb9d9e09f2210096243e8
Durzogg <caleb.miller@jdmsoft.com>
2024-08-13 13:28:30 -0600

    Kalman Filter Implementation in CPP File Changed to only Methods

--- src/kalman.cpp
+++ src/kalman.cpp
@@ -1,60 +1,45 @@
 #include "init.h"
+// Kalman Filter class methods
 
+// (private)
+void KalmanFilter::KalmanFilterLoop()
+{
+    // current measurement value
+    int currentMeasurement = 0;
 
-class KalmanFilter {
+    // current actual values
+    int currentHeading;
+    int currentCovariance; // initial guess
 
-    private:
-        // instance variables
-        pros::IMU* sensor; // defined in constructor
-        pros::Task* filterLoop_ptr; // starts when the Kalman filter turns on
+    // predicted measurement values for later in the cycle
+    int statePrediction = 0;
+    int estimateVariancePrediction = 0;
 
-        int filteredHeading; // updates as Kalman filter runs
-        int filterUncertainty; // updates as Kalman filter runs
+    // variable for the Kalman gain
+    int kalmanGain = 0;
 
-        int delay; // time between cycles
+    // standard deviation of the variances
+    int measurementDeviation = 0;
+    int predictionDeviation = 0;
 
-        std::vector<double> measurementVariances; // list of all measurement variances from the estimate
-        std::vector<double> predictionVariances; // list of all prediction variances from the estimate
-
-
-        void KalmanFilterLoop()
-        {
-            // current measurement value
-            int currentMeasurement = 0;
-
-            // current actual values
-            int currentHeading = 0;
-            int currentCovariance = 0; // initial guess
-
-            // predicted measurement values for later in the cycle
-            int statePrediction = 0;
-            int estimateVariancePrediction = 0;
-
-            // variable for the Kalman gain
-            int kalmanGain = 0;
-
-            // standard deviation of the variances
-            int measurementDeviation = 0;
-            int predictionDeviation = 0;
-
-            // velocity values
-            double velocity = 0;
-            pros::c::imu_accel_s_t rawAcceleration;
-            double gAcceleration = 0;
-            double msAcceleration = 0;
+    // velocity values
+    double velocity = 0;
+    pros::c::imu_accel_s_t rawAcceleration;
+    double gAcceleration = 0;
+    double msAcceleration = 0;
             
-            // initial loop
+    // initial loop
 
-            // measurement guess phase
-            currentHeading = sensor->get_heading(); // initial measurement, treated as "filtered" for starting cycle
-            int currentCovariance = 0; // initial guess
+    // measurement guess phase
+    currentHeading = sensor->get_heading(); // initial measurement, treated as "filtered" for starting cycle
+    currentCovariance = 0; // initial guess
 
-            // prediction phase
-            statePrediction = currentHeading + (velocity * (this->delay / 1000));; // State Extrapolation Equation
-            estimateVariancePrediction = currentCovariance + (std::pow((this->delay / 1000), 2) * predictionDeviation); // Covariance Extrapolation Equation
+    // prediction phase
+    statePrediction = currentHeading + (velocity * (this->delay / 1000));; // State Extrapolation Equation
+    estimateVariancePrediction = currentCovariance + (std::pow((this->delay / 1000), 2) * predictionDeviation); // Covariance Extrapolation Equation
 
-            // looping filter
-            while (true) {
+    // looping filter
+    while (true) {
                 // measurement phase
                 currentMeasurement = sensor->get_heading();
 
@@ -88,72 +73,70 @@ class KalmanFilter {
                 this->filteredHeading = currentHeading;
                 this->filterUncertainty = currentCovariance;
                 pros::delay(this->delay);
-            }
-        }
+    }
+}
 
 
 
-        double calculateStandardDeviation(
-            std::vector<double> listOfDifferences // list of differences from the estimates
-        ) 
-        {
-            double standardDeviation = 0;
+// calculates the standard deviation of a set of values, used for the filter (private)
+double KalmanFilter::calculateStandardDeviation(
+    std::vector<double> listOfDifferences // list of differences from the estimates
+) 
+{
+    double standardDeviation = 0;
 
-            for (int i = 0; i < listOfDifferences.size(); i++) {
-                standardDeviation += std::pow(listOfDifferences[i], 2); // squares each value's distance from its estimate to find the standard deviation
-            }
+    for (int i = 0; i < listOfDifferences.size(); i++) {
+        standardDeviation += std::pow(listOfDifferences[i], 2); // squares each value's distance from its estimate to find the standard deviation
+    }
 
-            double standardDeviation = standardDeviation / listOfDifferences.size();
-            return standardDeviation;
-        }
-
-
+    standardDeviation = standardDeviation / listOfDifferences.size();
+    return standardDeviation;
+}
 
 
-    public:
 
-        // constructor
-        KalmanFilter(pros::IMU* sensor) {
+// constructor (public)
+KalmanFilter::KalmanFilter(pros::IMU* sensor) {
 
-            // instance variable initializations
-            filterLoop_ptr = NULL; // sets the filter loop pointer to null so it can be defined later
-            filteredHeading = -1; // filtered heading is set to -1 as an unset value
-            filterUncertainty = -1; // filtered uncertainty is set to -1 as an unset value
+    // instance variable initializations
+    filterLoop_ptr = NULL; // sets the filter loop pointer to null so it can be defined later
+    filteredHeading = -1; // filtered heading is set to -1 as an unset value
+    filterUncertainty = -1; // filtered uncertainty is set to -1 as an unset value
 
-            delay = 5; // delay (in ms) between cycles
+    delay = 5; // delay (in ms) between cycles
 
-            this->sensor = sensor;
-        }
+    this->sensor = sensor;
+}
 
-        /* 
-        * returns the filtered heading that the filter provides,
-        * but will return -1 if the filter is not active
-        */
-        int getFilteredHeading()
-        {return filteredHeading;}
+/* 
+* returns the filtered heading that the filter provides,
+* but will return -1 if the filter is not active
+* (public)
+*/
+int KalmanFilter::getFilteredHeading()
+    {return filteredHeading;}
 
-        int getFilterUncertainty()
-        {return filterUncertainty;}
+// (public)
+int KalmanFilter::getFilterUncertainty()
+    {return filterUncertainty;}
 
-        // starts the filter loop if it is not already active
-        void startFilter() {
-            if (filterLoop_ptr == NULL) {
-                filterLoop_ptr = new pros::Task(KalmanFilterLoop);
-            }
-        }
+// starts the filter loop if it is not already active (public)
+void KalmanFilter::startFilter() {
+    if (filterLoop_ptr == NULL) {
+        filterLoop_ptr = new pros::Task(KalmanFilter::KalmanFilterLoop);
+    }
+}
 
-        // completely ends the filter loop if it is active
-        void endFilter() {
-            if (filterLoop_ptr != NULL) {
+// completely ends the filter loop if it is active (public)
+void KalmanFilter::endFilter() {
+    if (filterLoop_ptr != NULL) {
 
-                filterLoop_ptr->remove();
-                filterLoop_ptr = NULL;
+        filterLoop_ptr->remove();
+        filterLoop_ptr = NULL;
 
-                filteredHeading = -1;
+        filteredHeading = -1;
 
-                measurementVariances.clear();
-                predictionVariances.clear();
-            }
-        }
-
-};
+        measurementVariances.clear();
+        predictionVariances.clear();
+    }
+}
